0cf4208bdaaac4f912e784a7fd2a14394c04befc
singsing0526 <80672943+singsing0526@users.noreply.github.com>
2021-03-18 22:32:33 +0800

    GAME COTAIN MENU/PAUSE MENU/VOULUME SETTING/1/2/3/4 player SENCE
    GAME

---
+++ GAY/Assets/Lesson/Lesson06/Lesson06/CollectItems.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollectItems : MonoBehaviour {
+
+	public AudioClip coinSound;
+
+	void OnTriggerEnter(Collider col) {
+		if(col.tag == "Coin") {
+			Destroy(col.gameObject);
+			AudioSource.PlayClipAtPoint(coinSound, transform.position);
+		}
+	}
+}
