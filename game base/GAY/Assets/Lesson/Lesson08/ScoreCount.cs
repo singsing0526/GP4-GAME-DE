a3226333e1ebc409e20798bcc865f3613f85fbb7
singsing0526 <80672943+singsing0526@users.noreply.github.com>
2021-03-22 16:56:22 +0800

    GAME BASE

---
+++ game base/GAY/Assets/Lesson/Lesson08/ScoreCount.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ScoreCount : MonoBehaviour {
+
+	public static int score;
+	public static Text scoreText;
+
+	void Start () {
+		scoreText = GetComponent<Text>();
+		score = PlayerPrefs.GetInt("scoreData",0);
+		UpdateScore (0);
+	}
+	
+	public static void UpdateScore(int value) {
+		score += value;
+		PlayerPrefs.SetInt("scoreData", score);
+		PlayerPrefs.Save();
+		scoreText.text= "Score: " + score.ToString();
+	}
+}
+
+
+
---
+++ game base/GAY/Assets/quit.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class quit : MonoBehaviour
+{
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("menu");
+    }
+}
